783df7bc2b0a3d245b9eb72bbe937a47d64ab44e
EverPalestine <141242091+Ev3rPalestine@users.noreply.github.com>
2024-10-05 13:31:25 +0300

    Additionl features and improvement to filter.sh
    A- Adjusted output directory from "output" to "output/$website_input" to avoid overwriting previously filtered data and organize output results per website input.
    
    B- Added filtering status notes for open redirect, lfi and sqli. It was only for XXS filtering status only previously

--- filter.sh
+++ filter.sh
@@ -15,34 +15,37 @@ echo "Normalized URL being used: $website_url"
 
 # Create an output directory if it doesn't exist
 output_dir="output"
-mkdir -p "$output_dir"
+mkdir -p "$output_dir/"$website_input""
 
 # Step 1: Run katana with passive sources and save output to a unified file (output/output.txt)
 echo "Running katana with passive sources (waybackarchive, commoncrawl, alienvault)..."
-echo "$website_url" | katana -ps -pss waybackarchive,commoncrawl,alienvault -f qurl | uro > "$output_dir/output.txt"
+echo "$website_url" | katana -ps -pss waybackarchive,commoncrawl,alienvault -f qurl | uro > "$output_dir/"$website_input"/output.txt"
 
 # Step 2: Run katana actively with depth 5 and append results to output/output.txt
 echo "Running katana actively with depth 5..."
-katana -u "$website_url" -d 5 -f qurl | uro | anew "$output_dir/output.txt"
+katana -u "$website_url" -d 5 -f qurl | uro | anew "$output_dir/"$website_input"/output.txt"
 
 # Step 3: Filter output/output.txt for different vulnerabilities
 
 # XSS
 echo "Filtering URLs for potential XSS endpoints..."
-cat "$output_dir/output.txt" | Gxss | kxss | grep -oP '^URL: \K\S+' | sed 's/=.*/=/' | sort -u > "$output_dir/xss_output.txt"
-echo "Extracting final filtered URLs to $output_dir/xss_output.txt..."
+cat "$output_dir/"$website_input"/output.txt" | Gxss | kxss | grep -oP '^URL: \K\S+' | sed 's/=.*/=/' | sort -u > "$output_dir/xss_output.txt"
+echo "Extracting final filtered URLs to $output_dir/"$website_input"/xss_output.txt..."
 
 # Open Redirect
 echo "Filtering URLs for potential Open Redirect endpoints..."
-cat "$output_dir/output.txt" | gf or | sed 's/=.*/=/' | sort -u > "$output_dir/open_redirect_output.txt"
+cat "$output_dir/"$website_input"/output.txt" | gf or | sed 's/=.*/=/' | sort -u > "$output_dir/"$website_input"/open_redirect_output.txt"
+echo "Extracting final filtered URLs to $output_dir/"$website_input"/open_redirect_output.txt..."
 
 # LFI
 echo "Filtering URLs for potential LFI endpoints..."
-cat "$output_dir/output.txt" | gf lfi | sed 's/=.*/=/' | sort -u > "$output_dir/lfi_output.txt"
+cat "$output_dir/""$website_input""/output.txt" | gf lfi | sed 's/=.*/=/' | sort -u > "$output_dir/"$website_input"/lfi_output.txt"
+echo "Extracting final filtered URLs to $output_dir/"$website_input"/open_redirect_output.txt..."
 
 # SQLi
 echo "Filtering URLs for potential SQLi endpoints..."
-cat "$output_dir/output.txt" | gf sqli | sed 's/=.*/=/' | sort -u > "$output_dir/sqli_output.txt"
+cat "$output_dir/"$website_input"/output.txt" | gf sqli | sed 's/=.*/=/' | sort -u > "$output_dir/"$website_input"/sqli_output.txt"
+echo "Extracting final filtered URLs to $output_dir/"$website_input"/lfi_output.txt..."
 
 # Remove the intermediate file output/output.txt
 rm "$output_dir/output.txt"
